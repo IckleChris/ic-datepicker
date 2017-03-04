0cfb55a95db642ff66c06113640ab327df962577
Chris Brown <chris1990@live.co.uk>
2017-03-04 18:42:44 +0000

    Fix: excludes node_modules from bundled code to greatly reduce the generated bundle size

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,39 @@
 const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
+
 const config = {
   entry: path.resolve(__dirname, 'dist/index.js'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundles/ic-datepicker.bundle.js',
     libraryTarget: 'umd'
-  }
+  },
+  externals: [nodeExternals()],
+  plugins: [
+    new UglifyJsPlugin({
+      beautify: false,
+      output: {
+        comments: false
+      },
+      mangle: {
+        screw_ie8: true
+      },
+      compress: {
+        screw_ie8: true,
+        warnings: false,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+        negate_iife: false
+      },
+    })
+  ]
 };
 
 module.exports = config;
